54bedf3787807216e0331315ad62e33e5b00045c
Tomas Baca <klaxalk@gmail.com>
2019-01-14 19:24:18 +0100

    refactored physics, added pe

--- misc/photon_attenuation/main.py
+++ misc/photon_attenuation/main.py
@@ -20,10 +20,10 @@ rc('text', usetex=True)
 
 # energy of the incoming photon
 E_0_keV = [0.0027, 60, 511, 1460, 10000]
-E_0 = [conversions.energy_ev_to_J(E * 1000) for E in E_0_keV] # [J]
+E_0_J = [conversions.energy_ev_to_J(E * 1000) for E in E_0_keV] # [J]
 
 # scatterer thickness
-thickness = 0.001 # [m]
+thickness = 0.001
 
 a_list = []
 angles = []
@@ -34,11 +34,13 @@ abs_prob = []
 total_cross_section = []
 total_area = 0
 
-step = 0.1 # [deg]
-step_theta = conversions.deg2rad(step) # [rad]
-print("step_theta: {}".format(step_theta))
+step = conversions.deg2rad(0.1) # [rad]
 
-for energy_idx,energy in enumerate(E_0):
+material = materials.Si
+
+print("{} electron density: {} 1/cm^3 \n".format(material.name, material.electron_density/(100*100*100)))
+
+for energy_idx,energy in enumerate(E_0_J):
 
     sublist1 = []
     klein_nishina.append(sublist1)
@@ -50,26 +52,14 @@ for energy_idx,energy in enumerate(E_0):
     abs_prob.append(sublist4)
     total_cross_section.append(physics.comptonCrossSection(energy))
 
-    for i in np.arange(-180, 180+step, step):
+    for theta in np.arange(-m.pi, m.pi+step, step):
 
-      # scattering angle
-      theta = conversions.deg2rad(i) # [rad]
-      
       # Klein-Nishina formula
       compton_diff_cross_section = physics.comptonDiffCrossSection(energy, theta)
       klein_nishina[energy_idx].append(compton_diff_cross_section)
       
-      # # energy of the scattered photon
-      # E_p = energy * P
-      # # energy of the scattered photon in in keV
-      # E_p_keV = 0.001*energy_J_to_eV(E_p)
-      # # energy of the recoil electron
-      # E_e = energy * (1 - P)
-      # # energy of the scattered photon in in keV
-      # E_e_keV = 0.001*energy_J_to_eV(E_e)
-      
       # target solid angle
-      omega_1 = m.pi * m.sin(m.fabs(theta)) * step_theta
+      omega_1 = m.pi * m.sin(m.fabs(theta)) * step
 
       # did not work well for thick materials
       # prob = CdTe_e_density * compton_diff_cross_section * omega_1 * thickness
@@ -84,80 +74,112 @@ for energy_idx,energy in enumerate(E_0):
           total_area += omega_1
 
 # normalize sigma_normalized
-for energy_idx,energy in enumerate(E_0):
+for energy_idx,energy in enumerate(E_0_J):
 
     total = sum(sigma_normalized[energy_idx])
     sigma_normalized[energy_idx] = [x/total for x in sigma_normalized[energy_idx]]
 
 # convert to abs. prob
-for energy_idx,energy in enumerate(E_0):
+for energy_idx,energy in enumerate(E_0_J):
 
-    total_prob = 1 - np.exp(-materials.Si.electron_density * total_cross_section[energy_idx] * thickness)
+    total_prob = 1 - np.exp(-material.electron_density * total_cross_section[energy_idx] * thickness)
     abs_prob[energy_idx] = [x*total_prob for x in sigma_normalized[energy_idx]]
 
 sigma_marginalized = [[x/total_cross_section[sublist_idx] for x in sublist] for sublist_idx,sublist in enumerate(sigma_normalized)]
 # sigma_normalized_normalized = [[x/total_cross_section[sublist_idx] for x in sublist] for sublist_idx,sublist in enumerate(klein_nishina)]
 # sigma_normalized_normalized = [[x for x in sublist] for sublist_idx,sublist in enumerate(sigma_normalized)]
 
-for energy_idx,energy in enumerate(E_0):
+for energy_idx,energy in enumerate(E_0_J):
     summ = 0
     for idx,value in enumerate(sigma_normalized[energy_idx]):
         summ += value
-    print("prob_sum for {} keV: {}".format(energy, summ))
+    print("prob_sum for {} keV: {}".format(conversions.energy_J_to_eV(energy)/1000, summ))
 
-def plot_everything(*args):
+# #{ plot_everything()
+    
+    def plot_everything(*args):
+    
+        multiplot = False
+        plot_compton = False
+    
+        if plot_compton:
 
-    multiplot = False
+          fig = plt.figure(1)
+          fig.canvas.set_window_title("1")
+          if multiplot:
+              ax = plt.subplot(141, projection='polar')
+          else:
+              ax = plt.subplot(111, projection='polar')
+          for energy_idx,energy in enumerate(E_0_J):
+            ax.plot(angles, klein_nishina[energy_idx], label="{} keV".format(E_0_keV[energy_idx]))
+          ax.legend()
+          ax.grid(True)
+          plt.title("Compton scattering diff. cross section for $\heta \in [0, \pi]$".format())
+          plt.savefig("klein_nishina_1.png", bbox_inches="tight")
+          
+          if multiplot:
+              ax = plt.subplot(142, projection='polar')
+          else:
+              fig = plt.figure(2)
+              ax = plt.subplot(111, projection='polar')
+          for energy_idx,energy in enumerate(E_0_J):
+            ax.plot(angles, abs_prob[energy_idx], label="{} keV".format(E_0_keV[energy_idx]))
+          ax.grid(True)
+          ax.legend()
+          plt.title('Posterior prob. of scattering by a radial angle $\Theta \in [0, \pi]$'.format())
+          plt.savefig("klein_nishina_2.png", bbox_inches="tight")
+          
+          if multiplot:
+              ax = plt.subplot(143, projection='polar')
+          else:
+              fig = plt.figure(3)
+              ax = plt.subplot(111, projection='polar')
+          for energy_idx,energy in enumerate(E_0_J):
+            ax.plot(angles, sigma_normalized[energy_idx], label="{} keV".format(E_0_keV[energy_idx]))
+          ax.grid(True)
+          ax.legend()
+          plt.title('The likelihood of scattering by a radial angle $\Theta$, {} mm {}'.format(thickness*1000, material.name))
+          plt.savefig("klein_nishina_3.png", bbox_inches="tight")
+          plt.show()
 
-    fig = plt.figure(1)
-    fig.canvas.set_window_title("1")
-    if multiplot:
-        ax = plt.subplot(131, projection='polar')
-    else:
-        ax = plt.subplot(111, projection='polar')
-    for energy_idx,energy in enumerate(E_0):
-      ax.plot(angles, klein_nishina[energy_idx], label="{} keV".format(E_0_keV[energy_idx]))
-    ax.legend()
-    ax.grid(True)
-    plt.title("Compton scattering diff. cross section for $\heta \in [0, \pi]$".format())
-    plt.savefig("klein_nishina_1.png", bbox_inches="tight")
-
-    if multiplot:
-        ax = plt.subplot(132, projection='polar')
-    else:
-        fig = plt.figure(2)
-        ax = plt.subplot(111, projection='polar')
-    for energy_idx,energy in enumerate(E_0):
-      ax.plot(angles, abs_prob[energy_idx], label="{} keV".format(E_0_keV[energy_idx]))
-    ax.grid(True)
-    ax.legend()
-    plt.title('Posterior prob. of scattering by a radial angle $\Theta \in [0, \pi]$'.format())
-    plt.savefig("klein_nishina_2.png", bbox_inches="tight")
-
-    if multiplot:
-        ax = plt.subplot(133, projection='polar')
-    else:
-        fig = plt.figure(3)
-        ax = plt.subplot(111, projection='polar')
-    for energy_idx,energy in enumerate(E_0):
-      ax.plot(angles, sigma_normalized[energy_idx], label="{} keV".format(E_0_keV[energy_idx]))
-    ax.grid(True)
-    ax.legend()
-    plt.title('The likelihood of scattering by a radial angle $\Theta$, {} mm Si'.format(thickness*1000))
-    plt.savefig("klein_nishina_3.png", bbox_inches="tight")
-    plt.show()
+        fig = plt.figure(10)
+        ax = plt.subplot(111)
+        ax.plot(pe_energies, pe_prob_Si, label="PE-effect prob. Si".format())
+        ax.plot(pe_energies, pe_prob_CdTe, label="PE-effect prob. CdTe".format())
+        ax.grid(True)
+        ax.legend()
+        plt.show()
+    
+# #} end of plot_everything()
 
 for energy_idx,energy in enumerate(E_0_keV):
     print("")
     print("total_cross_section[energy_idx]: {0:1.2f} re^2".format(total_cross_section[energy_idx]/m.pow(constants.r_e, 2)))
 
-    prob = 1 - np.exp(-materials.Si.electron_density * total_cross_section[energy_idx] * thickness)
-    # prob = CdTe_e_density * total_cross_section[energy_idx] * thickness
+    prob = 1 - np.exp(-material.electron_density * total_cross_section[energy_idx] * thickness)
+    # prob = material.electron_density * total_cross_section[energy_idx] * thickness
 
-    print("{0:2.5f}% of photons are scattered for {1:6.1f} keV".format(prob, energy))
+    print("{0:2.1f}% of photons are scattered for {1:6.1f} keV".format(prob*100, energy))
 
 print("")
 print("total area: {0:2.3f} sr".format(total_area))
+
+pe_prob_Si = []
+pe_prob_CdTe = []
+pe_energies = []
+
+for e in range(1, 10000, 10):
+    pe_energies.append(e)
+
+    pecc_Si = physics.photoelectricEffectCrossSection(materials.Si, e*1000)
+    print("pecc_Si: {}".format(pecc_Si))
+    pecc_CdTe = physics.photoelectricEffectCrossSection(materials.CdTe, e*1000)
+
+    prob = 1 - np.exp(-materials.Si.electron_density * pecc_Si * thickness)
+    pe_prob_Si.append(prob)
+
+    prob = 1 - np.exp(-materials.CdTe.electron_density * pecc_CdTe * thickness)
+    pe_prob_CdTe.append(prob)
 
 pid = os.fork()
 if pid == 0:
--- misc/photon_attenuation/physics.py
+++ misc/photon_attenuation/physics.py
@@ -12,6 +12,15 @@ def comptonDiffCrossSection(energy, theta):
 
     P = comptonRatio(energy, theta)
 
+    # # energy of the scattered photon
+    # E_p = energy * P
+    # # energy of the scattered photon in in keV
+    # E_p_keV = 0.001*energy_J_to_eV(E_p)
+    # # energy of the recoil electron
+    # E_e = energy * (1 - P)
+    # # energy of the scattered photon in in keV
+    # E_e_keV = 0.001*energy_J_to_eV(E_e)
+
     return 0.5 * m.pow(P, 2) * m.pow(constants.r_e, 2) * (P + 1.0/P - m.pow(m.sin(theta), 2))
 
 def comptonCrossSection(energy):
@@ -33,11 +42,23 @@ def comptonCrossSection(energy):
     return total_cross_section
 
 # photoelectric effect coefficients
-pe_a = [1.6268e-9, -2.683e-12, 4.173e-2, 1]
-pe_b = [1.5274e-9, -5.110e-13, 1.027e-2, 2]
-pe_c = [1.1330e-9, -2.177e-12, 2.013e-2, 3.5]
-pe_p = [-9.1e10-1, 0, 0, 4]
+pe_a = [1.6268e-9, 1.5274e-9, 1.1330e-9, -9.12e-11]
+pe_b = [-2.683e-12, -5.110e-13, -2.177e-12, 0]
+pe_c = [4.173e-2, 1.027e-2, 2.013e-2, 0]
+pe_p = [1, 2, 3.5, 4]
 
-def photoelectricEffectCrossSection(energy):
+def photoelectricEffectCrossSection(material, energy):
+
+    e_rest_mass_energy = 5.11e5
+
+    k = energy/e_rest_mass_energy
     
-   pass 
+    if True or k < 0.9:
+        return (16/3)*m.sqrt(2)*m.pi*m.pow(constants.r_e, 2)*m.pow(constants.alpha, 4)*(m.pow(material.atomic_number, 5)/m.pow(k, 3.5))
+    else:
+        summ = 0
+        for i in range(0, 4):
+            summ += ((pe_a[i] + pe_b[i]*material.atomic_number)/(1 + pe_c[i]*material.atomic_number))*m.pow(k, -pe_p[i])
+            print("summ: {}".format(summ))
+
+        return m.pow(material.atomic_number, 5) * summ
