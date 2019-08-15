69d3339203f0d17db3ff68bb2eb46e4ff682a174
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-08-15 14:52:20 +0200

    added autocitations removal for my citations

--- thesis/citations/ppb.sh
+++ thesis/citations/ppb.sh
@@ -9,48 +9,66 @@ elif [ -x "$(whereis vim | awk '{print $2}')" ]; then
   HEADLESS=""
 fi
 
-FILES=`ls | grep .bib`
+FULL_DIR=full_list
+NO_AUTOCIT_DIR=no_autocit
+AUTHORS_DIR=author_list
+
+FILES=`ls $FULL_DIR | grep .bib`
 
 for file in $FILES; do
 
   INPUT_FILE=${file%.bib}
 
-  echo $INPUT_FILE
+  filename=$FULL_DIR/$file
 
-  $VIM_BIN $HEADLESS -nEs -c "%g/Booktitle/norm f{v%:s/\<\(\w\)\(\S*\)/\u\1\L\2/g" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%g/Journal/norm f{v%:s/\<\(\w\)\(\S*\)/\u\1\L\2/g" -c "wqa" -- "$file"
+  echo Pre-processing $filename
 
-  $VIM_BIN $HEADLESS -nEs -c "%g/^Unique-ID/norm ct=keywords ^f{C{$INPUT_FILE}," -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^doi/norm dd" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^isbn/norm dd" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^issn/norm dd" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^note/norm dd" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^editor/norm dd" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^series/norm dd" -c "wqa" -- "$file"
+  $VIM_BIN $HEADLESS -nEs -c "%g/Booktitle/norm f{v%:s/\<\(\w\)\(\S*\)/\u\1\L\2/g" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%g/Journal/norm f{v%:s/\<\(\w\)\(\S*\)/\u\1\L\2/g" -c "wqa" -- "$filename"
 
-  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^pages/norm :s/--/-/g" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^pages/norm :s/-/--/g" -c "wqa" -- "$file"
+  $VIM_BIN $HEADLESS -nEs -c "%g/^Unique-ID/norm ct=keywords ^f{C{$INPUT_FILE}," -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^doi/norm dd" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^isbn/norm dd" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^issn/norm dd" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^note/norm dd" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^editor/norm dd" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^series/norm dd" -c "wqa" -- "$filename"
 
-  $VIM_BIN $HEADLESS -nEs -c "%s/ieee/IEEE/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/rsj/RSJ/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/case/CASE/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/icuas/ICUAS/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/iros/IROS/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/icra/ICRA/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/etfa/ETFA/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/(irc/(IRC/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/ecmr/ECMR/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/spie/SPIE/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/ijcnn/IJCNN/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/icarcv/ICARCV/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/(ae)/(AE)/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/mbzirc/MBZIRC/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/Cern@school/CERN school/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/cern/CERN/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/vzlusat/VZLUSAT/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/euv/EUV/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/mesas/MESAS/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/rcar/RCAR/gi" -c "wqa" -- "$file"
-  $VIM_BIN $HEADLESS -nEs -c "%s/wrx-r/WRX-R/gi" -c "wqa" -- "$file"
+  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^pages/norm :s/--/-/g" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^pages/norm :s/-/--/g" -c "wqa" -- "$filename"
+
+  $VIM_BIN $HEADLESS -nEs -c "%s/ieee/IEEE/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/rsj/RSJ/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/case/CASE/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/icuas/ICUAS/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/iros/IROS/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/icra/ICRA/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/etfa/ETFA/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/(irc/(IRC/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/ecmr/ECMR/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/spie/SPIE/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/ijcnn/IJCNN/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/icarcv/ICARCV/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/(ae)/(AE)/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/mbzirc/MBZIRC/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/Cern@school/CERN school/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/cern/CERN/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/vzlusat/VZLUSAT/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/euv/EUV/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/mesas/MESAS/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/rcar/RCAR/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/wrx-r/WRX-R/gi" -c "wqa" -- "$filename"
+  $VIM_BIN $HEADLESS -nEs -c "%s/med-hoc-net/Med-Hoc-Net/gi" -c "wqa" -- "$filename"
+
+  filename2=$NO_AUTOCIT_DIR/$file
+  cp "$filename" "$filename2"
+
+  echo Removing second-level autocitations from $filename2
+
+  for author in `cat $AUTHORS_DIR/$INPUT_FILE.txt`; do
+
+    $VIM_BIN $HEADLESS -nEs -c "set ignorecase" -c "%g/^author.*$author/norm dap" -c "wqa" -- "$filename2"
+
+  done
 
 done
