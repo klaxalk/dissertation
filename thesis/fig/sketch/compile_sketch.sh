1596b19ce3684acc76ad53cd1eb56a4eaedccda5
Tomas Baca <tomas.baca@fel.cvut.cz>
2020-11-03 16:03:59 +0100

    added radiation paper

--- thesis/fig/sketch/compile_sketch.sh
+++ thesis/fig/sketch/compile_sketch.sh
@@ -4,6 +4,14 @@ APP_PATH=`( cd "$APP_PATH" && pwd )`
 
 cd "$APP_PATH"
 
+if [ -x "$(whereis nvim | awk '{print $2}')" ]; then
+  VIM_BIN="$(whereis nvim | awk '{print $2}')"
+  HEADLESS="--headless"
+elif [ -x "$(whereis vim | awk '{print $2}')" ]; then
+  VIM_BIN="$(whereis vim | awk '{print $2}')"
+  HEADLESS=""
+fi
+
 NAME=${1%.sk}
 
 sketch -T "$1" > "$NAME.tex"
@@ -16,13 +24,15 @@ if [ ! "$?" -eq 0 ]; then
   
 else
 
+  $VIM_BIN $HEADLESS -E -s -c "%g/amsmath/norm f{lct}amsmath,amsfonts,amssymb,bm" -c "wqa" -- "$NAME.tex"
+
   latex "$NAME.tex"
   rm "$NAME.tex"
 
   dvips -Ppdf -G0 "$NAME.dvi"
   rm "$NAME.dvi"
 
-  ps2pdf "$NAME.ps"
+  ps2pdf -dNOSAFER "$NAME.ps"
   pdfcrop "$NAME.pdf" "$NAME.pdf"
 
   rm "$NAME.ps"
@@ -31,3 +41,4 @@ else
   rm "texput.log"
   
 fi
+
