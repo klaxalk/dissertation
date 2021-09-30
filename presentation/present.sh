d4649c53472d9579676f81b526bcc292a81fb95f
Tomas Baca <klaxalk@gmail.com>
2021-09-30 11:08:56 +0200

    update

--- presentation/present.sh
+++ presentation/present.sh
@@ -14,7 +14,7 @@ NOTES=""
 
 TIMER="20"
 
-PAGE="27"
+PAGE=""
 
 #############################################################
 
@@ -59,6 +59,10 @@ ARG_PAGE=""
 if [ -n "$PAGE" ]; then
   ARG_PAGE="-P $PAGE"
 fi
+
+# /usr/bin/vim -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" $HEADLESS -E -s -c "%s/\\par/\\\n\\\n/g" -c "wqa" -- build/main.pdfpc
+# /usr/bin/vim -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" $HEADLESS -E -s -c "%s/\\\\/\\/g" -c "wqa" -- build/main.pdfpc
+# /usr/bin/vim -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" $HEADLESS -E -s -c "%g/\\\\n/\\n/g" -c "wqa" -- build/main.pdfpc
 
 cmd="~/git/pdfpc/build/bin/pdfpc $MONITORS --note-format=markdown $ARG_PAGE $ARG_NOTES $ARG_SINGLE_SCREEN $ARG_SWAP_SCREENS $ARG_TIMER $ARG_PERSIST_CACHE $ARG_DISABLE_CACHE ./build/main.pdf"
 echo $cmd
