67d5f075a5e37ffcdbe07ea072a6efd7c6029126
Tomas Baca <klaxalk@gmail.com>
2022-01-18 10:50:31 +0100

    minor changes

--- presentation/present.sh
+++ presentation/present.sh
@@ -8,12 +8,16 @@ PERSIST_CACHE="false"
 
 DISABLE_CACHE="false"
 
+# set the name for the monitors (use arandr to get them)
 # MONITORS="--presenter-screen=eDP-1 --presentation-screen=DP-1"
 
+# load special pdfpc notes
 NOTES=""
 
+# timer in minutes
 TIMER="20"
 
+# start from page #n
 PAGE=""
 
 #############################################################
@@ -59,10 +63,6 @@ ARG_PAGE=""
 if [ -n "$PAGE" ]; then
   ARG_PAGE="-P $PAGE"
 fi
-
-# /usr/bin/vim -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" $HEADLESS -E -s -c "%s/\\par/\\\n\\\n/g" -c "wqa" -- build/main.pdfpc
-# /usr/bin/vim -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" $HEADLESS -E -s -c "%s/\\\\/\\/g" -c "wqa" -- build/main.pdfpc
-# /usr/bin/vim -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" $HEADLESS -E -s -c "%g/\\\\n/\\n/g" -c "wqa" -- build/main.pdfpc
 
 cmd="~/git/linux-setup/submodules/pdfpc/build/bin/pdfpc $MONITORS --note-format=markdown $ARG_PAGE $ARG_NOTES $ARG_SINGLE_SCREEN $ARG_SWAP_SCREENS $ARG_TIMER $ARG_PERSIST_CACHE $ARG_DISABLE_CACHE ./build/main.pdf"
 echo $cmd
